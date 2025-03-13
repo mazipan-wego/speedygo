fb72c58a7957c2b8c1162fb2ab056c7d495210d2
Irfan Maulana <irfan.maulana@wego.com>
2025-03-13 11:44:11 +0700

    CHORE: Reduce the number of runs and frequesncy

--- _data/sites/all-wego.js
+++ _data/sites/all-wego.js
@@ -4,8 +4,8 @@ module.exports = {
 	// skip if localhost
 	skip: !process.env.CONTEXT,
 	options: {
-		run: 3,
-		frequency: 60 * 23, // 23 hours
+		run: 1,
+		frequency: 60 * 24 * 2, // per 2 days
 		// Use "run" if the sites don’t share assets on the same origin
 		//           and we can reset chrome with each run instead of
 		//           each site in every run (it’s faster)
